12975c7548cb9096c5faaaebf92ffd8c9e0c3902
Jérémy Martin <jmartin2019@1life.fr>
2020-10-12 16:58:12 +0200

    Manifest

---
+++ scoring/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Scoring',
+    'version': '0.1.0',
+    'author': 'MyFab',
+    'license': 'Open-prod license',
+    'summary': 'Score les entreprises',
+    'category': 'Partner',
+    'description': """Permet de connaître la solvabilité des entreprises.""",
+    'website': 'https://www.myfab.fr/',
+    'module_type': 'optional',
+    'images': [],
+    'depends': [
+        'base_openprod'
+    ],
+    'data': [],
+    'qweb': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
