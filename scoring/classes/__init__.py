7bbe974144a9fb147ad78596a1f3aa9d80f1a6ac
Jeremy <jmartin@1life.fr>
2020-10-14 17:05:30 +0200

    DEVWEB-739 Ajout boutons refresh + load from Intuiz

--- scoring/classes/__init__.py
+++ scoring/classes/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 import res_partner
+import wizard_load_from_intuiz
+import wizard_refresh_from_intuiz
---
+++ scoring/classes/wizard_load_from_intuiz.py
@@ -0,0 +1,17 @@
+from openerp import models, fields, api, _
+
+class wizard_load_from_intuiz(models.TransientModel):
+    _name = 'wizard.partner.load.intuiz'
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(wizard_load_from_intuiz, self).default_get(fields_list=fields_list)
+        res['partner_ids'] = self.env.context.get('active_ids')
+        return res
+
+    # ===========================================================================
+    # COLUMNS
+    # ===========================================================================
+    name = fields.Char(string='Name', size=32, required=False)
+    partner_ids = fields.Many2many('res.partner', 'wizard_partner_load_from_intuiz_res_partner_rel', 'wiz_load_id', 'partner_id',
+                                   string='Partner', copy=False)
