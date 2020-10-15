c3cde8796fe95e7e472a5830973ffb77eae1a67f
unknown <kcharignon@1life.fr>
2020-10-15 14:20:00 +0200

    Creation des class intuiz

--- scoring/classes/__init__.py
+++ scoring/classes/__init__.py
@@ -2,3 +2,5 @@
 import res_partner
 import wizard_load_from_intuiz
 import wizard_refresh_from_intuiz
+import intuiz_api
+import intuiz_api_search
---
+++ scoring/classes/intuiz_api_search.py
@@ -0,0 +1,54 @@
+import intuiz_api
+import requests
+from openerp import models, fields, api, _
+
+class Search():
+    # Constructeur
+    def __init__(self,
+                 intuizApi,
+                 where,
+                 who
+                 ):
+        self.where = where
+        self.who = who
+        self.intuiz_api = intuizApi
+        self.body = """
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://serviceobject.service.callisto.newsys.altares.fr" xmlns:xsd="http://request.callisto.newsys.altares.fr/xsd">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <ser:doRechercheSimple>
+         <!--Optional:-->
+         <ser:request>
+            <!--Optional:-->
+            <xsd:identification>U2019004798|6c0dcf59bb30429d8ed449048e3ef6fe689cff9a</xsd:identification>
+            <!--Optional:-->
+            <xsd:refClient>recherche</xsd:refClient>
+            <!--Optional:-->
+            <xsd:categorieItemADeselectionner>1</xsd:categorieItemADeselectionner>
+            <!--Optional:-->
+            <xsd:categorieItemId></xsd:categorieItemId>
+            <!--Optional:-->
+            <xsd:contexteRecherche></xsd:contexteRecherche>
+            <!--Optional:-->
+            <xsd:debutResultat>0</xsd:debutResultat>
+            <!--Optional:-->
+            <xsd:elargie>1</xsd:elargie>
+            <!--Optional:-->
+            <xsd:nbElt>200</xsd:nbElt>
+            <!--Optional:-->
+            <xsd:ou>69</xsd:ou>
+            <!--Optional:-->
+            <xsd:qui>1Life</xsd:qui>
+            <!--Optional:-->
+            <xsd:rechercheActif>0</xsd:rechercheActif>
+            <!--Optional:-->
+            <xsd:rechercheSiege>0</xsd:rechercheSiege>
+         </ser:request>
+      </ser:doRechercheSimple>
+   </soapenv:Body>
+</soapenv:Envelope>
+        """
+
+    def send(self):
+        request = requests.post(self.intuiz_api.url, headers=self.intuiz_api.headers, payload={"body":self.body})
+        return request, request.content
