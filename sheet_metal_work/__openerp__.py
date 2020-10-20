81a9177e97509ca074b07c2132a3860d4cf95b75
unknown <kcharignon@1life.fr>
2020-10-20 11:21:53 +0200

    ajout des __init__.py

---
+++ sheet_metal_work/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Tôlerie',
+    'version' : '0.1.0',
+    'author' : 'MyFab',
+    'license': 'Open-prod license',
+    'summary': 'Modules tôlerie',
+    'category' : 'Metier',
+    'description' : """Regroupe les modules necessaire pour la tôlerie.""",
+    'website': 'https://www.myfab.fr/',
+    'module_type': 'advanced',
+    'images' : [],
+    'depends' : [
+        'my_fab'
+    ],
+    'data': [],
+    'qweb' : [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
