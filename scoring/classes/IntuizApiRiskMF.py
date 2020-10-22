b313d37623c4457456ae2b8607a5ab6c89a0023d
JeremyMartin <jmartin@1life.fr>
2020-10-22 09:52:55 +0200

    DEVWEB-739 IntuizApiRiskMF

---
+++ scoring/classes/IntuizApiRiskMF.py
@@ -0,0 +1,14 @@
+from openerp import models, fields, api, _
+from IntuizApiMF import IntuizApiMF
+
+
+class IntuizApiRiskMF(models.Model):
+    _name = "intuiz.api.risk.mf"
+    _inherit = "intuiz.api.mf"
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(IntuizApiRiskMF, self).default_get(fields_list=fields_list)
+        # TODO : instantiate Risk uri
+        res["uri_mf"] = "https://" + res["host_mf"] + "/iws-v3.18/services/CallistoIdentiteObjectSecure.CallistoIdentiteObjectSecureHttpsSoap11Endpoint/"
+        return res
