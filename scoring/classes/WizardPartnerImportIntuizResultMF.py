97036734c6bb6407695729c829c986f4821699cb
JeremyMartin <jmartin@1life.fr>
2020-10-28 17:27:46 +0100

    DEVWEB-778 WIP transformation res_partner_temp choisis en res_partner

--- scoring/classes/WizardPartnerImportIntuizResultMF.py
+++ scoring/classes/WizardPartnerImportIntuizResultMF.py
@@ -11,17 +11,22 @@ class WizardPartnerImportIntuizResultMF(models.TransientModel):
     name = fields.Char(string="Name", size=32, required=False)
     where_mf = fields.Char(string="Where", size=128, required=False)
     who_mf = fields.Char(string="Who", size=128, required=False)
-    res_partner_temp_ids = fields.Many2many('res.partner.temp.mf', 'wizard_partner_import_intuiz_mf_res_partner_temp_mf_rel',
+    res_partner_temps = fields.Many2many('res.partner.temp.mf', 'wizard_partner_import_intuiz_mf_res_partner_temps_mf_rel',
                                             'wiz_import_id', 'partner_temp_id', string='Partner', copy=False, readonly=False)
 
     @api.model
     def default_get(self, fields_list):
         res = super(WizardPartnerImportIntuizResultMF, self).default_get(fields_list=fields_list)
         intuiz_api_identity = self.env["intuiz.api.identity.mf"].create({})
-        res_partner_temp_ids = intuiz_api_identity.getPartnersTemp(self.env.context.get('where'), self.env.context.get('who'))
-        res["res_partner_temp_ids"] = res_partner_temp_ids
+        res_partner_temps = intuiz_api_identity.getPartnersTemp(self.env.context.get('where'), self.env.context.get('who'))
+        res["res_partner_temps"] = res_partner_temps
         return res
 
     @api.multi
     def action_validate(self):
         print("VALIDATE ACTION")
+        for partner_temp in self.res_partner_temps:
+            if partner_temp.selected:
+                # TODO : create res_partner fr
+                print("")
+
