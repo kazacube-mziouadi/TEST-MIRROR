cc41d980934cff696c663f45410bc862095d517d
unknown <kcharignon@1life.fr>
2020-10-30 12:10:36 +0100

    Many2One ScoreHistory => ResPartner

--- scoring/classes/ResPartner.py
+++ scoring/classes/ResPartner.py
@@ -11,7 +11,7 @@ class ResPartner(models.Model, IntuizMapInterfaceMF):
     # ===========================================================================
     # Adds column score in the partner
     score_mf = fields.Integer(string="Score", default=0, required=False)
-    score_history_mf = fields.One2many("score.mf", "partner_id_mf", copy=False)
+    # score_history_mf = fields.One2many("score.mf", "partner_id_mf", copy=False)
 
     # ===========================================================================
     # METHODS
