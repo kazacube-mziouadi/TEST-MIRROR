d2fe3cd73d51b506bb0528e4aea8c045e648a995
unknown <kcharignon@1life.fr>
2020-11-02 12:28:37 +0100

    test

--- scoring/classes/IntuizApiRiskMF.py
+++ scoring/classes/IntuizApiRiskMF.py
@@ -17,21 +17,23 @@ class IntuizApiRiskMF(models.TransientModel):
     def get_score_history(self, partner):
         siren = partner.siret_number[0:9]
         response = self.send(IntuizApiBodyRiskGetScoreHistoryMF(self.user_mf, self.password_mf, siren))
-        print("IntuizApiRiskMF.19")
-        print(response)
+        print("IntuizApiRiskMF.20")
+        print(repr(partner));
         print("IntuizApiRiskMF.22")
-        response_parsed = ET.fromstring(response)
+        print(response)
         print("IntuizApiRiskMF.24")
-        element_my_info = response_parsed[0][0][0].find("{http://response.callisto.newsys.altares.fr/xsd}myInfo")
+        response_parsed = ET.fromstring(response)
         print("IntuizApiRiskMF.26")
+        element_my_info = response_parsed[0][0][0].find("{http://response.callisto.newsys.altares.fr/xsd}myInfo")
+        print("IntuizApiRiskMF.28")
         print(element_my_info)
         score_history_api = element_my_info.findall("{http://risque.vo.callisto.newsys.altares.fr/xsd}scoreList")
-        print("IntuizApiRiskMF.29")
+        print("IntuizApiRiskMF.31")
         print(score_history_api)
 
         score_history_temp = []
         for score_api in score_history_api:
-            print("IntuizApiRiskMF.33")
+            print("IntuizApiRiskMF.36")
             print(score_api)
             score_temp = self.env["score.mf"].create({
                 "score_cent_mf": score_api.find("{http://risque.vo.callisto.newsys.altares.fr/xsd}scoreCent").text,
