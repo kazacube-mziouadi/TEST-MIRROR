188213b950c6bf0fca4682f111540427eedaed78
unknown <kcharignon@1life.fr>
2020-11-02 12:01:41 +0100

    test

--- scoring/classes/IntuizApiRiskMF.py
+++ scoring/classes/IntuizApiRiskMF.py
@@ -56,11 +56,11 @@ class IntuizApiRiskMF(models.TransientModel):
         # print(element_my_info)
         # print(element_my_info.text)
 
-
-        print("IntuizApiRiskMF.60")
-        element_my_info = response_parsed[0][0][0].find("ax297:myInfo")
-        print(element_my_info)
-        print(element_my_info.text)
+        # THIS METHOD DIDN'T WORK (prefixe ax297 not found in prefix map)
+        # print("IntuizApiRiskMF.60")
+        # element_my_info = response_parsed[0][0][0].find("ax297:myInfo")
+        # print(element_my_info)
+        # print(element_my_info.text)
 
 
         print("IntuizApiRiskMF.66")
