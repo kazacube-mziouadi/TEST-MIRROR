565a669a1156dc758e1e8cd9cb07570c0139e5c7
JeremyMartin <jmartin@1life.fr>
2020-11-02 11:29:52 +0100

    DEVWEB-778 Refresh from Intuiz OK

--- scoring/classes/WizardPartnerRefreshIntuizMF.py
+++ scoring/classes/WizardPartnerRefreshIntuizMF.py
@@ -2,17 +2,31 @@ from openerp import models, fields, api, _
 
 
 class WizardPartnerRefreshIntuizMF(models.TransientModel):
-    _name = 'wizard.partner.refresh.intuiz.mf'
+    _name = "wizard.partner.refresh.intuiz.mf"
 
     # ===========================================================================
     # COLUMNS
     # ===========================================================================
-    name = fields.Char(string='Name', size=32, required=False)
-    partner_ids = fields.Many2many('res.partner', 'wizard_partner_refresh_intuiz_mf_res_partner_temp_mf_rel',
-                                   'wiz_refresh_id', 'res_partner_temp_id', string='Partner', copy=False)
+    name = fields.Char(string="Name", size=32, required=False)
 
     @api.model
     def default_get(self, fields_list):
         res = super(WizardPartnerRefreshIntuizMF, self).default_get(fields_list=fields_list)
-        res['partner_ids'] = self.env.context.get('active_ids')
+        intuiz_api_identity = self.env["intuiz.api.identity.mf"].create({})
+        print(self.env.context.get("active_ids"))
+        for partner_selected_id in self.env.context.get("active_ids"):
+            partner_selected = self.env["res.partner"].search([["id", "=", partner_selected_id]], None, 1)
+            print(partner_selected)
+            res_partner_temps = intuiz_api_identity.getPartnersTemp(partner_selected.zip,
+                                                                    partner_selected.siret_number)
+            res_partner_temp = self.env["res.partner.temp.mf"].search([["id", "=", res_partner_temps[0]]], None, 1)
+            print(res_partner_temp)
+            partner_selected.write({
+                "name": res_partner_temp.name,
+                "score_mf": res_partner_temp.score_mf,
+                "street": res_partner_temp.street_mf,
+                "city": res_partner_temp.city_mf,
+                "zip": res_partner_temp.zip_mf,
+                "reference": res_partner_temp.siret_mf
+            })
         return res
