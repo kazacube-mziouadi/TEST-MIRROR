55cca910b12b70d13e872428c04839c8fafc0d2b
JeremyMartin <jmartin@1life.fr>
2020-12-04 11:07:55 +0100

    DEVWEB-832 Test unitaire Scoring WIP

--- scoring/tests/__init__.py
+++ scoring/tests/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-import classes
+import test_scoring
---
+++ scoring/tests/test_scoring.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from openerp.tests.common import TransactionCase
+from openerp.tools.translate import _
+
+class test_scoring(TransactionCase):
+    """
+        Tests d'import et mise a jour des donnees de client via Intuiz.
+    """
+
+    def setUp(self):
+        super(test_scoring, self).setUp()
+        self.partner_model = self.env["res.partner"].create({
+            "name": "TEST_PARTNER",
+            "zip": "38",
+            "country": "Afghanistan",
+            "is_customer": True,
+            "siret_number": "35197753300022",
+            "reference": self.env['ir.sequence'].get('res.partner')
+        })
+
+    def test_update_partner_data(self):
+        intuiz_api_identity = self.env["intuiz.api.identity.mf"].create({})
+        res_partner_temps = intuiz_api_identity.get_partners_temp(self.partner_model.zip,
+                                                                  self.partner_model.siret_number)
+        res_partner_temp = self.env["res.partner.temp.mf"].search([["id", "=", res_partner_temps[0]]], None, 1)
+        self.partner_model.write(self.env["res.partner"].create_from_object_temp(self, res_partner_temp, False))
+        self.assertEquals(self.partner_model.zip, "38082", _("The SIRET isn't right"))
