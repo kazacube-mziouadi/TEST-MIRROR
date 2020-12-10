5d00d5adb2de017e8dfc0278ddf3c84a5a436c21
JeremyMartin <jmartin@1life.fr>
2020-12-10 16:47:29 +0100

    DEVWEB-838 corrections mineures

--- my_fab/WizardInstallerMF.py
+++ my_fab/WizardInstallerMF.py
@@ -10,5 +10,7 @@ class WizardInstallerMF(models.TransientModel):
     name = fields.Char(string="Name", size=32, required=False)
 
     def trigger_wizard(self, cr, uid, context=None):
-        print("WIZARD TRIGGERED")
+        print("WIZARD TRIGGERED MF")
 
+    def action_validate_config(self):
+        pass
