5ec72311dfd8879e37422a34c0fd5462b230f769
JeremyMartin <jmartin@1life.fr>
2020-12-10 11:14:19 +0100

    DEVWEB-838 WIP myfab install

---
+++ my_fab/installer.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+try:
+    import simplejson as json
+except ImportError:
+    import json     # noqa
+
+from openerp.osv import fields, osv
+
+_logger = logging.getLogger(__name__)
+
+class myfab_installer(osv.osv_memory):
+    _name = 'myfab.installer'
+    _inherit = 'res.config.installer'
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        if context is None: context = {}
+        res = super(myfab_installer, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar, submenu=False)
+        print("INSTALLING")
+        return res
+
+    def execute(self, cr, uid, ids, context=None):
+        self.execute_simple(cr, uid, ids, context)
+        return super(myfab_installer, self).execute(cr, uid, ids, context=context)
+
+    def execute_simple(self, cr, uid, ids, context=None):
+        print("EXECUTING SIMPLE")
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
