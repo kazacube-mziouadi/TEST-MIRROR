0ff43ef905afe5cfae791f20cf617479f719a70e
qcharignon <qcharignon@1life.fr>
2021-01-13 12:08:29 +0100

    DEVWEB-870 GED WIP

--- ged_simplified/DocumentDirectory.py
+++ ged_simplified/DocumentDirectory.py
@@ -12,8 +12,13 @@ class DocumentDirectory(models.Model):
 
 
     def put_documents(self, documents):
+        print("####DocumentDirectory::put_document - in")
         for document in documents:
+            print("########DocumentDirectory::put_document - for : " + document.name)
             if document.directory_id != self:
+                print("############DocumentDirectory::put_document - for : " + document.name + " write.")
                 document.write({
                     "directory_id": self.id
                 })
+            else:
+                print("############DocumentDirectory::put_document - for : " + document.name + " not write.")
