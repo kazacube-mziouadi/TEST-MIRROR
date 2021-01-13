bc569f0339cd6498a32bb053a9cb566605e7bcd4
qcharignon <qcharignon@1life.fr>
2021-01-13 17:35:32 +0100

    DEVWEB-870 WIP

--- ged_simplified/DocumentOpenProd.py
+++ ged_simplified/DocumentOpenProd.py
@@ -11,10 +11,7 @@ class DocumentOpenProd(models.Model):
     def compute_link_document(self, document_path, directory):
         created_timestamp = time.ctime(os.path.getctime(document_path))
         last_modified_timestamp = time.ctime(os.path.getmtime(document_path))
-        month_number = datetime.fromtimestamp(os.path.getmtime(document_path)).strftime('%m')
-        year_number = datetime.fromtimestamp(os.path.getmtime(document_path)).strftime('%Y')
-        print("last modified: %s" % time.ctime(os.path.getmtime(document_path)))
-        print("created: %s" % time.ctime(os.path.getctime(document_path)))
+        last_modified_date = datetime.fromtimestamp(os.path.getmtime(document_path))
         print(document_path)
         filename = os.path.basename(document_path)
         with open(document_path, 'r') as f:
@@ -28,14 +25,15 @@ class DocumentOpenProd(models.Model):
             "directory_id": directory.id,
             "create_date": created_timestamp,
             "write_date": last_modified_timestamp,
-            "month": month_number,
-            "year": year_number
+            "month": last_modified_date.strftime('%m'),
+            "year": last_modified_date.strftime('%Y'),
+            "date": last_modified_date.strftime('%Y-%m-%d')
         }
         self.env.cr.execute('''
                         INSERT INTO
                             document_openprod
-                        (name, extension, index_content, full_path, directory_id, create_date, write_date, month, year, write_uid, create_uid, user_id, company_id, state)
-                        VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')
+                        (name, extension, index_content, full_path, directory_id, create_date, write_date, month, year, write_uid, create_uid, user_id, company_id, state, date, button_save_visible)
+                        VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s', %s)
                     ''' % (
                 file_attributes["name"],
                 file_attributes["extension"],
@@ -50,6 +48,9 @@ class DocumentOpenProd(models.Model):
                 self._uid,
                 self._uid,
                 self.env.user.company_id.id,
-                "draft"
+                "draft",
+                file_attributes["date"],
+                "False"
+
             )
         )
