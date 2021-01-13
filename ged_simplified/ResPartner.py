a898f9718a62173be039e5427a04eb6f2ddb1621
qcharignon <qcharignon@1life.fr>
2021-01-13 14:16:48 +0100

    DEVWEB-870 GED WIP

--- ged_simplified/ResPartner.py
+++ ged_simplified/ResPartner.py
@@ -40,9 +40,10 @@ class ResPartner(models.Model):
                 "parent_id": partners_directory.id,
                 "active": True
             })
-        self.write({
-            "directory_id_mf": partner_directory.id
-        })
+        # self.write({
+        #     "directory_id_mf": partner_directory.id
+        # })
+        self.compute_directory(partner_directory)
         return partner_directory
 
     def put_documents_in_current_directory(self):
