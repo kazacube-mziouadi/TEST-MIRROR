3016982ecbee7b3a470a2a2a97cc83196b8c27b0
jmartin <jmartin@1life.fr>
2021-05-10 14:58:08 +0200

    DEVWEB-1013 Droits module WipSim

--- wipsim/__openerp__.py
+++ wipsim/__openerp__.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name' : 'WipSim Interface',
-    'version' : '0.1.0',
-    'author' : 'MyFab',
+    'name': 'WipSim Interface',
+    'version': '0.1.0',
+    'author': 'MyFab',
     'license': 'Open-prod license',
     'summary': 'Interface pour échanger avec le logiciel de pilotage de production WipSim',
-    'category' : 'Base',
-    'description' : """Interface pour échanger avec le logiciel de pilotage de production WipSim.""",
+    'category': 'Base',
+    'description': """Interface pour échanger avec le logiciel de pilotage de production WipSim.""",
     'website': 'https://www.myfab.fr/',
     'module_type': 'base',
-    'images' : [],
-    'depends' : [
+    'images': [],
+    'depends': [
         'my_fab',
         'affair'
     ],
     'data': [
+        'security/wipsim_security.xml',
+        'security/ir.model.access.csv',
         'views/wipsim.xml',
         'views/baseMenus.xml'
     ],
-    'qweb' : [],
+    'qweb': [],
     'demo': [],
     'test': [],
     'installable': True,
