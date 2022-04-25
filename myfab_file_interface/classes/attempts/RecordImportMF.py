5f91d78a8136369484fcd11a80505872c8550102
jmartin <jmartin@1life.fr>
2022-04-25 17:01:09 +0200

    IS-33 Corrections trads

--- myfab_file_interface/classes/attempts/RecordImportMF.py
+++ myfab_file_interface/classes/attempts/RecordImportMF.py
@@ -12,8 +12,8 @@ class RecordImportMF(models.AbstractModel):
     name = fields.Char(string="Name", size=64, help='')
     method_mf = fields.Char(string="Method applied")
     model_mf = fields.Many2one("ir.model", string="Model")
-    fields_mf = fields.Char(string="Record's fields")
-    fields_to_write_mf = fields.Char(string="Record's fields to write")
+    fields_mf = fields.Text(string="Record's fields")
+    fields_to_write_mf = fields.Text(string="Record's fields to write")
     status_mf = fields.Selection(
         [("not processed", _("Not processed")), ("success", _("Success")), ("failed", _("Failed")), ("ignored", _("Ignored"))],
         "Status", default="not processed", readonly=True
