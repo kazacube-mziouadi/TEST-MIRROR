2d5a98110839bf27f6244870f62f12995ee34cb7
jmartin <jmartin@1life.fr>
2022-05-10 09:56:29 +0200

    IS-159 WIP champs ligne de simulation

--- myfab_simulation_by_quantity/models/MFSimulationByQuantityLine.py
+++ myfab_simulation_by_quantity/models/MFSimulationByQuantityLine.py
@@ -10,3 +10,9 @@ class MFSimulationByQuantityLine(models.Model):
     # ===========================================================================
     name = fields.Char(string="Name", size=64, readonly=True)
     mf_simulation_id = fields.Many2one("mf.simulation.by.quantity", string="Simulation", required=True)
+    mf_quantity = fields.Float(string="Quantity")
+    mf_price_material = fields.Float(string="Material price")
+    mf_price_consumable = fields.Float(string="Consumable price")
+    mf_price_subcontracting = fields.Float(string="Subcontracting price")
+    mf_price_workforce = fields.Float(string="Workforce price")
+    mf_general_costs = fields.Float(string="General costs")
