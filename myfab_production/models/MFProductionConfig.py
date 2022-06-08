40f80e11a9ff76476a9cf0b068fca79eed5065b4
jmartin <jmartin@1life.fr>
2022-06-08 17:30:52 +0200

    IS-143 Prise en charge fuseau horaire + booléen activation + trads

--- myfab_production/models/MFProductionConfig.py
+++ myfab_production/models/MFProductionConfig.py
@@ -16,14 +16,29 @@ class MFProductionConfig(models.Model):
     # ===========================================================================
     # COLUMNS
     # ===========================================================================
-    name = fields.Char(string="Name", default=_("myfab production configuration"), readonly=True)
+    name = fields.Char(string="Name", default=lambda self: self._get_default_name(), readonly=True)
+    mf_use_default_end_time = fields.Boolean(
+        string="Use default end time", default=False,
+        help="Activate the use of the below default end time in the Create Timetracking wizard"
+    )
     mf_default_end_time = fields.Float(string="Default end time", help="Format HH:MM")
+    mf_default_end_time_timezone_name = fields.Char(string="Default end timezone name", store=True,
+                                                    compute="_compute_mf_default_end_time_timezone_name")
 
     # ===========================================================================
     # METHODS
     # ===========================================================================
+    @staticmethod
+    def _get_default_name():
+        return _("myfab production configuration")
+
     @api.one
     @api.constrains("mf_default_end_time")
     def _check_mf_default_end_time(self):
         if self.mf_default_end_time >= 24:
             raise ValidationError(_("The time tracking's default end time can not be greater than 23:59"))
+
+    @api.one
+    @api.depends("mf_default_end_time")
+    def _compute_mf_default_end_time_timezone_name(self):
+        self.mf_default_end_time_timezone_name = self.env.user.company_id.tz
--- myfab_production/models/WizardCreateTimetracking.py
+++ myfab_production/models/WizardCreateTimetracking.py
@@ -1,5 +1,5 @@
 from openerp import models, fields, api, _, modules
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 
@@ -16,25 +16,43 @@ class WizardCreateTimetracking(models.Model):
     # ===========================================================================
     def _get_default_datetime(self):
         mf_production_config_id = self.env["mf.production.config"].search([], None, 1)
+        use_config_default_end_time = mf_production_config_id.mf_use_default_end_time
         config_default_end_time = mf_production_config_id.mf_default_end_time
-        if not config_default_end_time:
+        config_default_end_time_timezone_name = mf_production_config_id.mf_default_end_time_timezone_name
+        if not use_config_default_end_time:
             return fields.Datetime.now()
-        today = datetime.today().strftime("%Y-%m-%d")
-        default_end_time_hours_str = self.get_hours_str_from_time_float(config_default_end_time)
-        default_end_time_minutes_str = self.get_minutes_str_from_time_float(config_default_end_time)
-        company_timezone = pytz.timezone(self.env.user.company_id.tz)
-        return company_timezone.fromutc(datetime.strptime(
-            today + ' ' + default_end_time_hours_str + ':' + default_end_time_minutes_str, "%Y-%m-%d %H:%M"
-        )).strftime("%Y-%m-%d %H:%M:%S")
+        config_default_end_time_timezone_offset_int = self.get_timezone_offset_int(config_default_end_time_timezone_name)
+        today_str = datetime.today().strftime("%Y-%m-%d")
+        end_time_hours_str = self.get_hours_str_from_time_float(config_default_end_time)
+        end_time_minutes_str = self.get_minutes_str_from_time_float(config_default_end_time)
+        end_datetime_utc0 = datetime.strptime(
+            today_str + ' ' + end_time_hours_str + ':' + end_time_minutes_str, "%Y-%m-%d %H:%M"
+        )
+        end_datetime_correct_utc = end_datetime_utc0 - timedelta(hours=config_default_end_time_timezone_offset_int)
+        return end_datetime_correct_utc.strftime("%Y-%m-%d %H:%M:%S")
 
     @staticmethod
     def get_hours_str_from_time_float(time_float):
+        # Get integer part of float
         default_end_time_integer_part_float = time_float // 1
         default_end_time_hours_int = int(default_end_time_integer_part_float)
         return str(default_end_time_hours_int)
 
     @staticmethod
     def get_minutes_str_from_time_float(time_float):
+        # Get decimal part of float
         default_end_time_decimal_part_float = time_float % 1
         default_end_time_minutes_int = int(round(60 * default_end_time_decimal_part_float))
         return str(default_end_time_minutes_int)
+
+    # Returns the timezone offset as an int
+    @staticmethod
+    def get_timezone_offset_int(timezone):
+        # The offset at format +01:00 or -02:00 for example
+        offset_str = str(datetime.now(pytz.timezone(timezone)))[-6:]
+        operator_str = offset_str[0]
+        offset_hours_int = int(offset_str[1:3])
+        if operator_str == '+':
+            return offset_hours_int
+        else:
+            return -offset_hours_int
