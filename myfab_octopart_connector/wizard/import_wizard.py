8f1a09a167c75f979e986d94b5249078d235939b
jmartin <jmartin@1life.fr>
2022-06-20 12:18:30 +0200

    correction et ajout du code fabricant

--- myfab_octopart_connector/wizard/import_wizard.py
+++ myfab_octopart_connector/wizard/import_wizard.py
@@ -25,7 +25,6 @@ class octopart_import_wizard_template(models.TransientModel):
     part_uid_octopart = fields.Char(required=True)
     manufacturer_name = fields.Char(string="Manufacturer name")
     manufacturer_uid_octopart = fields.Char(string="Manufacturer Octopart id")
-    manufacturer_code = fields.Char(string="Manufacturer code")
     manufacturer_url = fields.Char(string="Manufacturer url")
     list_seller_offers_ids = fields.One2many('octopart.seller.offer', 'import_wizard_template_id', string='Offers')
     product_template_id = fields.Many2one('product.product', 'Product template', required=True, ondelete='cascade')
@@ -44,7 +43,75 @@ class octopart_import_wizard_template(models.TransientModel):
             if self.product_template_id.company_id:
                 self.company_id = self.product_template_id.company_id.id
 
-    @api.multi
+    @api.one
+    def import_details(self):
+        #API request
+        search_result = self.env['octopart.api'].get_api_data(self._set_data())
+        if search_result and len(search_result['data']['parts']) > 0:
+            search_response = search_result['data']['parts'][0]
+            currency_octopart = ''
+            price_octopart = ''
+            item_number = ''
+            
+            # Write request result to wizard
+            if 'sellers' in search_response:
+                sellers = search_response['sellers']
+
+            url_datasheet = False
+            if search_response['best_datasheet'] != None and 'url' in search_response['best_datasheet']:
+                url_datasheet = search_response['best_datasheet']['url']
+            
+            url_image = False
+            if len(search_response['images']) > 0 and 'url' in search_response['images'][0]:
+                url_image = search_response['images'][0]['url']
+            
+            self.write({
+                'datasheet' : url_datasheet,
+                'image' : url_image,
+                'manufacturer_name' : search_response['manufacturer']['name'],
+                'manufacturer_uid_octopart' : search_response['manufacturer']['id'],
+                'manufacturer_url' : search_response['manufacturer']['homepage_url'],
+                })
+
+            for seller in sellers:
+                # Check if seller is in openprod
+                search_value = self.env['res.partner'].search_count([['octopart_uid_seller', '=', int(seller['company']['id'])], ])
+                if search_value > 0:
+                    company_value = seller['company']                 
+                    for offer in seller['offers']:
+                        # Check if order delay exist
+                        if offer['factory_lead_days']:
+                            order_delay_total = int(offer['factory_lead_days'])
+                            week_number = order_delay_total // 7
+                            order_delay = week_number * 5 + order_delay_total%7
+                        else:
+                            order_delay = None
+
+                        add_seller_offer  = self.env['octopart.seller.offer'].create({
+                            'name' :company_value['name'], 
+                            'seller_identifier' : company_value['id'], 
+                            'sku' : offer['sku'],
+                            #'octopart_currency' : currency_octopart,
+                            'oder_delay' : order_delay,
+                            'import_wizard_template_id' : self.id
+                        })
+                        # Create seller offer in wizard
+                        for price in offer['prices']:
+                            # Create price offer in wizard
+                            add_price_offer  = self.env['octopart.price.offer'].create({
+                                'offer_id' : add_seller_offer.id,
+                                'currency' : price['currency'],
+                                'price' : price['price'],
+                                'number_item' : price['quantity']
+                            })
+                    add_seller_offer.refresh()
+                    
+                    #TODO : refresh all wizard
+            return {
+                'type': 'ir.actions.act_window_no_close'
+            }
+
+    @api.one
     def import_product_template_wizard(self):
         search_result = self.env['octopart.search.result'].browse(self.env.context.get('active_id'))
         
@@ -52,11 +119,7 @@ class octopart_import_wizard_template(models.TransientModel):
         photo = False
         if self.image:
             photo = base64.b64encode(requests.get(self.image).content)
-        
-        file_datasheet = False
-        if self.datasheet:
-            file_datasheet = base64.b64encode(requests.get(self.datasheet).content)
-        
+                
         # Check if manufacturer is already in openprod
         search_manufacturer = self.env['octopart.manufacturer'].search([['octopart_uid', '=', self.manufacturer_uid_octopart], ])
         if search_manufacturer:
@@ -76,24 +139,24 @@ class octopart_import_wizard_template(models.TransientModel):
                                                                                           'octopart_uid_product': self.part_uid_octopart, 
                                                                                           'picture': photo, 
                                                                                           'manufacturer_id' : id_manufacturer,
-                                                                                          'manufacturer_code' : self.manufacturer_code,
+                                                                                          'manufacturer_code' : search_result.mpn,
                                                                                           })
         
         # Create attachement for datasheet
-        now = datetime.now()
-        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-
-        add_datasheet  = self.env['document.openprod'].create({
-            'name' : 'datasheet-%s'%date_time,
-            'extension' : 'pdf',
-            'fname' : self.part_uid_octopart,
-            'attachment' : file_datasheet,
-            'version' : '1',
-            'directory_id' : self.directory_id.id,
-                      
-        })
-        # Add datasheet to product
-        new_product_rc.write({'internal_plan_ids': [(4, add_datasheet.id, False)]})
+        if self.datasheet:
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+            file_datasheet = base64.b64encode(requests.get(self.datasheet).content)
+            add_datasheet  = self.env['document.openprod'].create({
+                'name' : 'datasheet-%s'%date_time,
+                'extension' : 'pdf',
+                'fname' : self.part_uid_octopart,
+                'attachment' : file_datasheet,
+                'version' : '1',
+                'directory_id' : self.directory_id.id,        
+            })
+            # Add datasheet to product
+            new_product_rc.write({'internal_plan_ids': [(4, add_datasheet.id, False)]})
 
         for offer in self.list_seller_offers_ids:
             # Check if seller is in openprod
@@ -118,80 +181,14 @@ class octopart_import_wizard_template(models.TransientModel):
                         'min_qty' : price_offer.number_item,
                         'price' : price_offer.price,
                     })
-        search_result.write({'is_in_openprod' : True})
-        
+                            
         for characteristic in self.product_id.value_ids:
             # Add characteristic to product
             characteristic.write({'product_id' : new_product_rc.id})
             characteristic.write({'result_id' : None})
 
         self.write({'product_id' : False})
-        return True
-
-    @api.multi
-    def import_details(self):
-        #API request
-        search_result = self.env['octopart.api'].get_api_data(self._set_data())
-        if search_result:
-            search_response = search_result['data']['parts'][0]
-            currency_octopart = ''
-            price_octopart = ''
-            item_number = ''
-            
-            # Write request result to wizard
-            url_datasheet = False
-            url_image = False
-            if 'sellers' in search_response:
-                sellers = search_response['sellers']
-            if search_response['best_datasheet'] != None:
-                if 'url' in search_response['best_datasheet']:
-                    url_datasheet = search_response['best_datasheet']['url']
-            if len(search_response['images']) > 0 and 'url' in search_response['images'][0]:
-                url_image = search_response['images'][0]['url']
-            
-            self.write({
-                'datasheet' : url_datasheet,
-                'image' : url_image,
-                'manufacturer_name' : search_response['manufacturer']['name'],
-                'manufacturer_uid_octopart' : search_response['manufacturer']['id'],
-                'manufacturer_url' : search_response['manufacturer']['homepage_url'],
-                })
-            for seller in sellers:
-                # Check if seller is in openprod
-                search_value = self.env['res.partner'].search_count([['octopart_uid_seller', '=', int(seller['company']['id'])], ])
-                if search_value > 0:
-                    company_value = seller['company']                 
-                    for offer in seller['offers']:
-                        # Check if order delay exist
-                        if offer['factory_lead_days']:
-                            order_delay_total = int(offer['factory_lead_days'])
-                            week_number = order_delay_total // 7
-                            order_delay = week_number * 5 + order_delay_total%7
-                        else:
-                            order_delay = None
-                        add_seller_offer  = self.env['octopart.seller.offer'].create({
-                            'name' :company_value['name'], 
-                            'seller_identifier' : company_value['id'], 
-                            'sku' : offer['sku'],
-                            #'octopart_currency' : currency_octopart,
-                            'oder_delay' : order_delay,
-                            'import_wizard_template_id' : self.id
-                        })
-                        # Create seller offer in wizard
-                        for price in offer['prices']:
-                            # Create price offer in wizard
-                            add_price_offer  = self.env['octopart.price.offer'].create({
-                                'offer_id' : add_seller_offer.id,
-                                'currency' : price['currency'],
-                                'price' : price['price'],
-                                'number_item' : price['quantity']
-                            })
-                    add_seller_offer.refresh()
-
-            return {
-                'type': 'ir.actions.act_window_no_close'
-            }
-    
+        return True   
       
     #méthode envoie et récupération de donnée serveur
     def _set_data(self):
@@ -233,7 +230,7 @@ class octopart_import_wizard_template(models.TransientModel):
             manufacturer{
               name
               id
-                homepage_url
+              homepage_url
             }
           }
         }
