67c58f868358dc5c78e685e4360cfcccc3a869cb
nginhoux <nginhoux@1life.fr>
2022-06-28 17:15:25 +0200

    Creation module

---
+++ myfab_multi_company/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'myfab Multi société',
+    'version' : '1.0.0',
+    'author' : 'myfab',
+    'license': 'Open-prod license',
+    'summary': 'Multi société par myfab',
+    'category' : 'Third party management',
+    'description' : """
+        Gestion plus complète de la fonctionnalité de multi société
+    """,
+    'website': 'https://www.myfab.fr/',
+    'module_type': 'advanced',
+    'depends' : [
+        'my_fab',
+        'multi_company_auto',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'persistent': True,
+}
