eda8cc85ac842ca98f4fbb7a7b0a2026c317156e
Jérémy Martin <jmartin@1life.fr>
2022-07-06 15:35:32 +0000

    IS-366 WIP validation simulation

--- cao_connector/classes/xml_import_processing.py
+++ cao_connector/classes/xml_import_processing.py
@@ -8,9 +8,19 @@ class xml_import_processing(models.Model):
     # ===========================================================================
     # METHODS
     # ===========================================================================
-
     def create_simulate_import(self, history):
         """
         Create list of simulate action of import.
         """
         self.write({"processing_simulate_action_ids": history})
+
+    @api.multi
+    def file_analyse(self):
+        if self.state == "sim":
+            self.import_simulation_lines()
+        else:
+            super(xml_import_processing, self).file_analyse()
+
+    def import_simulation_lines(self):
+        for simulation_line_id in self.processing_simulate_action_ids:
+            simulation_line_id.process_data_import()
--- cao_connector/classes/xml_import_processing_sim_action.py
+++ cao_connector/classes/xml_import_processing_sim_action.py
@@ -11,7 +11,24 @@ class xml_import_processing_sim_action(models.Model):
     mf_beacon_id = fields.Many2one("xml.import.beacon.relation", string="Beacon relation")
     mf_field_setter_ids = fields.One2many("mf.field.setter", "mf_model_dictionary_id", string="Field setters",
                                           help="Values to set non-relational fields with at simulation's validation.")
-    mf_sim_action_parent = fields.Many2one("xml.import.processing.sim.action", string="Parent simulation element",
-                                           ondelete="cascade")
-    mf_sim_action_children = fields.One2many("xml.import.processing.sim.action", "mf_sim_action_parent",
-                                             string="Children simulation elements")
+    mf_sim_action_parent_id = fields.Many2one("xml.import.processing.sim.action", string="Parent simulation element",
+                                              ondelete="cascade")
+    mf_sim_action_children_ids = fields.One2many("xml.import.processing.sim.action", "mf_sim_action_parent",
+                                                 string="Children simulation elements")
+
+    # ===========================================================================
+    # METHODS
+    # ===========================================================================
+    def process_data_import(self):
+        if self.type in ["create", "update"]:
+            fields_dict = {
+                field_setter_id.mf_field.name: field_setter_id.mf_value for field_setter_id in self.mf_field_setter_ids
+            }
+            for sim_action_child_id in self.mf_sim_action_children_ids:
+                child_record_id = sim_action_child_id.process_data_import()
+                fields_dict[sim_action_child_id.mf_beacon_id.relation_openprod_field_id.name] = [(4, child_record_id.id)]
+            if self.type == "create":
+                return self.env[self.mf_beacon_id.relation_openprod_id.model].create(fields_dict)
+            elif self.type == "update":
+                self.reference.write(fields_dict)
+                return self.reference
