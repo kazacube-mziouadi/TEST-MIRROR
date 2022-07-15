5fb8fc83fc85babe4e1e80ef1b40ff30a2014ba6
Jérémy Martin <jmartin@1life.fr>
2022-07-15 08:33:41 +0000

    US-378 Historique simulation OK

--- cao_connector/classes/xml_import_processing.py
+++ cao_connector/classes/xml_import_processing.py
@@ -13,12 +13,6 @@ class xml_import_processing(models.Model):
     # ===========================================================================
     # METHODS
     # ===========================================================================
-    @api.onchange("state")
-    def onchange_state(self):
-        print("ONCHANGE")
-        # if self.state != "done" and self.mf_imported_from_simulation:
-        #     self.mf_imported_from_simulation = False
-
     def create_simulate_import(self, history):
         """
         Create list of simulate action of import.
@@ -30,10 +24,27 @@ class xml_import_processing(models.Model):
         if self.state == "sim":
             self.import_simulation_lines()
             self.wkf_processing_done()
-            self.mf_imported_from_simulation = True
         else:
             super(xml_import_processing, self).file_analyse()
 
     def import_simulation_lines(self):
         for simulation_line_id in self.processing_simulate_action_ids:
             simulation_line_id.process_data_import()
+
+    @api.multi
+    def wkf_processing_wait(self):
+        self.write({"mf_imported_from_simulation": False})
+        return super(xml_import_processing, self).wkf_processing_wait()
+
+    @api.multi
+    def wkf_processing_done(self):
+        if self.state == "sim":
+            self.write({"state": "done", "error_message": "", "mf_imported_from_simulation": True})
+            return True
+        else:
+            return super(xml_import_processing, self).wkf_processing_done()
+
+    @api.multi
+    def clear_history(self, history):
+        self.processing_simulate_action_ids.unlink()
+        super(xml_import_processing, self).clear_history(history)
