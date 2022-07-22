5f2851813abf936acd82fb4393edb72e9ed579bd
nginhoux <nginhoux@1life.fr>
2022-07-22 17:07:40 +0200

    Changement d'optique de fonctionnement (mise de coté du kit et ajout de la gestion des étiquettes)

--- myfab_multi_company/__openerp__.py
+++ myfab_multi_company/__openerp__.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name' : 'myfab kits multi société',
+    'name' : 'myfab multi société',
     'version' : '1.0.0',
     'author' : 'myfab',
     'license': 'Open-prod license',
-    'summary': 'kits multi société par myfab',
+    'summary': 'multi société par myfab',
     'category' : 'Third party management',
     'description' : """
-        Gestion plus complète des kits en multi société
+        Gestion plus complète des échanges en multi société
     """,
     'website': 'https://www.myfab.fr/',
     'module_type': 'advanced',
     'depends' : [
         'my_fab',
         'multi_company_auto',
-        'kit_purchase',
-        'kit_sale',
-        'stock_kit_product',
-        'purchase',
     ],
     'data': [
         'views/purchase_order.xml',
@@ -26,5 +22,5 @@
     'installable': True,
     'auto_install': False,
     'application': True,
-    'persistent': True,
+    'persistent': False,
 }
