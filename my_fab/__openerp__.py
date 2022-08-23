d11a88a1e9e4c3c66a2bdee2adeb1fe07e85e459
nginhoux <nginhoux@1life.fr>
2022-08-23 16:16:51 +0200

    incrément version suite à correction trad

--- my_fab/__openerp__.py
+++ my_fab/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'myfab Base',
-    'version': '1.2.0',
+    'version': '1.2.1',
     'author': 'myfab',
     'license': 'Open-prod license',
     'summary': 'Initialisation modules et données pour myfab',
