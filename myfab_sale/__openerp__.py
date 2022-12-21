1a082a17e9e4270d28edf6fdd574e549b0a62325
dabgrall <dabgrall@1life.fr>
2022-12-21 10:11:14 +0100

    [IMP] creation module myfabs_sale + fonction recalcul regime fiscale

---
+++ myfab_sale/__openerp__.py
@@ -0,0 +1,28 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'myfab Vente',
+    'version': '1.0.0',
+    'author': 'myfab',
+    'license': 'Open-prod license',
+    'category': 'base',
+    'description': """
+        Intègre des options pour le module de vente 
+    """,
+    'summary': """
+        Intègre des options pour le module de vente
+    """,
+    'images': [],
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'qweb': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'not_show': False,
+}
